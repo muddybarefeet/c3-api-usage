11efffad6610ae7603c2bc7f85e31604ecfca4e1
Anna Rogers <2muddybarefeet@gmail.com>
2016-09-27 16:35:29 -0700

    react boilerplate

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+// The back of the cards
+const POKEBALL = "http://vignette3.wikia.nocookie.net/youtubepoop/images/4/4c/Pokeball.png/revision/latest"
+
+// The front of the cards
+const BULBASAUR = "http://cdn.bulbagarden.net/upload/2/21/001Bulbasaur.png"
+const CHARMANDER = "http://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/600px-004Charmander.png"
+const SQUIRTLE = "http://cdn.bulbagarden.net/upload/thumb/3/39/007Squirtle.png/250px-007Squirtle.png"
+const PIKACHU = "http://cdn.bulbagarden.net/upload/0/0d/025Pikachu.png"
+const JIGGLYPUFF = "http://cdn.bulbagarden.net/upload/thumb/3/3e/039Jigglypuff.png/250px-039Jigglypuff.png"
+const ABRA = "http://cdn.bulbagarden.net/upload/6/62/063Abra.png"
+const GYARADOS = "http://cdn.bulbagarden.net/upload/4/41/130Gyarados.png"
+const MEWTWO = "http://cdn.bulbagarden.net/upload/thumb/7/78/150Mewtwo.png/250px-150Mewtwo.png"
+
+
+class App extends Component {
+
+  constructor() {
+   super();
+   this.state = {
+     
+   };
+  }
+
+
+
+  render() {
+
+
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
